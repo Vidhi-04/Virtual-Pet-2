37eac0c684f04f5c1cb51776554a4ef8fbaa8449
Vidhi <77286305+Vidhi-04@users.noreply.github.com>
2021-03-28 19:52:01 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -37,23 +37,20 @@ function setup() {
 
 function draw() {  
   background(46, 139, 87)
-  fedTime = database.ref('FeedTime')
-  fedTime.on("value", function(data){
-    lastFed = data.val()
-  })
-  //add styles here
-  drawSprites();
-  fill(255, 255, 254)
-  textSize(15)
-  if(lastFed >= 12){
-    text("Last Fed : "+lastFed%12+" PM", 350, 30)
-  }
-  else if(lastFed === 0){
-    text("Last Fed : AM", 350, 30)
-  }
-  else{
-    text("Last Fed : "+lastFed+" AM", 350, 30)
-  }
+ fedTime=database.ref('FeedTime');
+  fedTime.on("value",function(data){
+    lastFed=data.val();
+  });
+ 
+  fill(255,255,254);
+  textSize(15);
+  if(lastFed>=12){
+    text("Last Feed : "+ lastFed%12 + " PM", 350,30);
+   }else if(lastFed==0){
+     text("Last Feed : 12 AM",350,30);
+   }else{
+     text("Last Feed : "+ lastFed + " AM", 350,30);
+   }
 foodObj.display();
 }
 
